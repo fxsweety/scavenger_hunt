557ab4581b47b462dbf586aa45479faf7e76d1b4
Bertilla <sweetybertilla_francisxavier@cable.comcast.com>
2018-10-03 21:15:24 -0400

    Second commit

---
+++ app/src/main/java/com/teambuilding/scavenger/QRCodeScannerActivity.java
@@ -0,0 +1,90 @@
+package com.teambuilding.scavenger;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+
+public class QRCodeScannerActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final int REQUEST_CAMERA_PERMISSION = 20;
+    private IntentIntegrator qrScan;
+    Button capture;
+    private TextView scanResults;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.barcode_scanner);
+
+        capture = findViewById(R.id.capture);
+        scanResults = findViewById(R.id.scan_results);
+
+        findViewById(R.id.cameraView).setVisibility(View.GONE);
+        qrScan = new IntentIntegrator(this);
+        capture.setOnClickListener(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CAMERA_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    qrScan.initiateScan();
+
+//                    Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+//                    intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+//                    startActivityForResult(intent, 0);
+                } else {
+                    Toast.makeText(QRCodeScannerActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        //initiating the qr code scan
+        ActivityCompat.requestPermissions(QRCodeScannerActivity.this, new String[]{android.Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+
+//        if (intent != null) {
+//            String contents = intent.getStringExtra("SCAN_RESULT");
+//            //if qrcode has nothing in it
+//            if (contents == null) {
+//                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+//            } else {
+//                //if qr contains data
+//                scanResults.setText(contents);
+//            }
+//        } else {
+//            super.onActivityResult(requestCode, resultCode, intent);
+//        }
+
+        if(result != null) {
+            //if qrcode has nothing in it
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                //if qr contains data
+                scanResults.setText(result.getContents());
+            }
+        }
+    }
+}
+
+
